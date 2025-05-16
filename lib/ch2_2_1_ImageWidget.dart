f09b5c3a5237856835a0951b15cb65d3351855ee
jungbini <jungbini@gmail.com>
2025-05-17 01:25:50 +0900

    feat: Switch Image widget to NetworkImage for testing

--- lib/ch2_2_1_ImageWidget.dart
+++ lib/ch2_2_1_ImageWidget.dart
@@ -44,11 +44,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center( 
-          child: Image(
-            // pubspec.yaml 에 assets/images/ 폴더 등록이 필요함
-            image: AssetImage('assets/images/pic.jpg'),            
-            // image: NetworkImage('https://pouriahosseini.gallerycdn.vsassets.io/extensions/pouriahosseini/omga-auto-commit/1.2.3/1741430181362/Microsoft.VisualStudio.Services.Icons.Default')
-          ),
+          child:
+            // Image.asset('assets/images/pic.jpg'),
+            Image.network('https://pouriahosseini.gallerycdn.vsassets.io/extensions/pouriahosseini/omga-auto-commit/1.2.3/1741430181362/Microsoft.VisualStudio.Services.Icons.Default')
         ),  
       )     
     );
