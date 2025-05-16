ff409b664d376469883dd0442c354ea8e7084af3
jungbini <jungbini@gmail.com>
2025-05-17 00:43:35 +0900

    Commit!

--- lib/ch2_1_5_ContainerWidget.dart
+++ lib/ch2_1_5_ContainerWidget.dart
@@ -24,14 +24,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          Container(
+        body: Center( 
+          child: Container(
             width: 200,
             height: 100,
-            
-          ) ,  
-        )
-      )
+            padding: EdgeInsets.all(8.0),
+            margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+            decoration: BoxDecoration( 
+              color: Colors.blue,
+              border: Border.all(color: Colors.black, width: 3),
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7, 
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Text(
+              'Hello, Container!',
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            )
+          ),
+        ),  
+      )     
     );
   }
 }
