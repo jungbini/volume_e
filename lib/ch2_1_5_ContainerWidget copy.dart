ca79affbca41283368454cd3ef8f5dba06180f96
jungbini <jungbini@gmail.com>
2025-05-17 01:11:50 +0900

    feat: Implement Container widget with styling and decoration

---
+++ lib/ch2_1_5_ContainerWidget copy.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+// 1단계
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+          
+//         )
+//       )
+//     );
+//   }
+// }
+
+// 2단계
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center( 
+//           child: Container(
+//             width: 200,
+//             height: 100,
+//             padding: EdgeInsets.all(8.0),
+//             margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+            
+//             child: Text(
+//               'Hello, Container!',
+//               style: TextStyle(color: Colors.black, fontSize: 24),
+//             )
+//           ),
+//         ),  
+//       )     
+//     );
+//   }
+// }
+
+// 3단계
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center( 
+          child: Container(
+            width: 200,
+            height: 100,
+            padding: EdgeInsets.all(8.0),
+            margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+
+            // 데코레이션 추가
+            decoration: BoxDecoration( 
+              color: Colors.blue,
+              border: Border.all(color: Colors.black, width: 3),
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7, 
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+
+            // 폰트 색깔을 흰색으로
+            child: Text(
+              'Hello, Container!',
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            )
+          ),
+        ),  
+      )     
+    );
+  }
+}
