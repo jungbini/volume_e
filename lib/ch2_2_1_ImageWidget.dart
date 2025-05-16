57501f3891c102097aac7cc8f7210e3d76c6897d
jungbini <jungbini@gmail.com>
2025-05-17 01:24:50 +0900

    feat: Implement Image widget example - Step 2

--- lib/ch2_2_1_ImageWidget.dart
+++ lib/ch2_2_1_ImageWidget.dart
@@ -18,6 +18,25 @@ void main() => runApp(MyApp());
 // }
 
 // 2단계
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center( 
+//           child: Image(
+//             // pubspec.yaml 에 assets/images/ 폴더 등록이 필요함
+//             image: AssetImage('assets/images/pic.jpg'),            
+//             // image: NetworkImage('https://pouriahosseini.gallerycdn.vsassets.io/extensions/pouriahosseini/omga-auto-commit/1.2.3/1741430181362/Microsoft.VisualStudio.Services.Icons.Default')
+//           ),
+//         ),  
+//       )     
+//     );
+//   }
+// }
+
+// 3단계
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -27,7 +46,7 @@ class MyApp extends StatelessWidget {
         body: Center( 
           child: Image(
             // pubspec.yaml 에 assets/images/ 폴더 등록이 필요함
-            image: AssetImage('assets/images/pic.jpg'),
+            image: AssetImage('assets/images/pic.jpg'),            
             // image: NetworkImage('https://pouriahosseini.gallerycdn.vsassets.io/extensions/pouriahosseini/omga-auto-commit/1.2.3/1741430181362/Microsoft.VisualStudio.Services.Icons.Default')
           ),
         ),  
@@ -35,18 +54,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// 3단계
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         body: Center( 
-          
-//         ),  
-//       )     
-//     );
-//   }
-// }
