675fb500bdff2c3182dde67425fc1ac5dbb55dc5
Jung-Been Lee <jungbini@gmail.com>
2025-05-17 15:23:00 +0900

    feat: Use Image.asset for local image and set BoxFit

--- lib/ch2_2_1_image_widget.dart
+++ lib/ch2_2_1_image_widget.dart
@@ -58,16 +58,15 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Image(
-            // pubspec.yaml 에 assets/images/ 폴더 등록이 필요함
-            image: AssetImage('assets/images/pic.jpg'),
-            // image: NetworkImage('https://pouriahosseini.gallerycdn.vsassets.io/extensions/pouriahosseini/omga-auto-commit/1.2.3/1741430181362/Microsoft.VisualStudio.Services.Icons.Default')
-          ),
+          child: Image.asset('assets/images/pic.jpg', fit: BoxFit.cover),
+          // Image.network('https://pouriahosseini.gallerycdn.vsassets.io/extensions/pouriahosseini/omga-auto-commit/1.2.3/1741430181362/Microsoft.VisualStudio.Services.Icons.Default')
         ),
-      )
+      ),
     );
   }
+}
